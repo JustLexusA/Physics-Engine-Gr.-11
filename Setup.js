622c07b27790d47b23ba2d987e1553da6910f275
JustAndrei <mugman121309@gmail.com>
2026-05-21 13:34:52 -0400

    Balls for testing

--- Setup.js
+++ Setup.js
@@ -20,6 +20,7 @@ function MyWorld() {
     var engine = Engine.create(),
         world = engine.world;
     // Change gravity
+    engine.gravity.y = 1
 
     // Create a renderer
     var render = Render.create({
@@ -34,6 +35,24 @@ function MyWorld() {
     });
     // Create the canvas, this adds the renderer's canvas which uses the p5.js drawing tools to render over the physics world.
     createCanvas(windowWidth, windowHeight, render.canvas)
+
+    // All sprites and objects
+    cueball = Bodies.circle(innerWidth / 2, innerHeight / 2, 20, {
+        density: 0.01,
+        friction: 0.05,
+        frictionAir: 0.01,
+        restitution: 0.95
+    });
+
+    staticBall = Bodies.circle(innerWidth / 2 + 10, (innerHeight / 2) + 100, 30, {
+        isStatic: true,
+        density: 0.05
+    });
+
+
+
+    // Composite
+    Composite.add(engine.world, [cueball, staticBall])
 
     // Run the renderer, this lets time pass to calculate the physics of the bodies inside the composite rather than letting them render in one frame forever.
     Render.run(render);
